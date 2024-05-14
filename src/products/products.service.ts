103ccdb4cae0bbfde758f06a54c264ec0b9d9f72
Breno <brenocartoon@hotmail.com>
2024-05-14 14:24:10 -0300

    feat: add simple product creation

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,36 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ProductDto } from './dto/product-dto';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  products: ProductDto[] = [
+    {
+      id: 1,
+      name: 'Pizza',
+      price: 10,
+    },
+    {
+      id: 2,
+      name: 'Burger',
+      price: 5,
+    },
+    {
+      id: 3,
+      name: 'Pasta',
+      price: 8,
+    },
+    {
+      id: 4,
+      name: 'Ice Cream',
+      price: 2,
+    },
+  ];
+
+  create(product: CreateProductDto) {
+    this.products.push(product);
+    return this.products;
   }
 
   findAll() {
-    return [
-      {
-        id: 1,
-        name: 'Pizza',
-        price: 10,
-      },
-      {
-        id: 2,
-        name: 'Burger',
-        price: 5,
-      },
-      {
-        id: 3,
-        name: 'Pasta',
-        price: 8,
-      },
-      {
-        id: 4,
-        name: 'Ice Cream',
-        price: 2,
-      },
-    ];
+    return this.products;
   }
 
   findOne(id: number) {
